c4305cf792a25e456844472e03aa2935b590b356
Adrian Rączka <raczka.tlen2@o2.pl>
2025-10-15 22:05:46 +0200

    Next lesson ended, commit to 02-Intermediate

--- 02-Intermediate/k_List_Comprehension/main.py
+++ 02-Intermediate/k_List_Comprehension/main.py
@@ -1,3 +1,18 @@
 numbers = [1, 2, 3]
 new_list = [n+1 for n in numbers]
 print(new_list)
+
+name = "Angela"
+new_list = [letter for letter in name]
+print(new_list)
+
+new_list = [n*2 for n in range(1, 5)]
+print(new_list)
+
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+new_list = [short_names for short_names in names if len(short_names) <= 4]
+print(new_list)
+
+new_list = [short_names.upper()
+            for short_names in names if len(short_names) > 5]
+print(new_list)
