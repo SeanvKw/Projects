50466f9763357c5da3179f68ba5594a1b9deb3b3
Adrian Rączka <104779541+SeanvKw@users.noreply.github.com>
2025-10-15 03:50:33 +0000

    Add inputs – personalized letter generator using templates

--- 02-Intermediate/h_Inputs/main.py
+++ 02-Intermediate/h_Inputs/main.py
@@ -4,15 +4,15 @@
 # Save the letters in the folder "ReadyToSend".
 PLACEHOLDER = "[name]"
 inv = []
-for line in open("projekty/21_inputs/Input/Names/invited_names.txt").readlines():
+for line in open("02-Intermediate/h_inputs/Input/Names/invited_names.txt").readlines():
     names = line.strip("\n")
     inv.append(names)
 
 for each_name in inv:
-    listy = open("projekty/21_inputs/Input/Letters/starting_letter.txt")
+    listy = open("02-Intermediate/h_inputs/Input/Letters/starting_letter.txt")
     listy_contents = listy.read()
     new_letter = listy_contents.replace(PLACEHOLDER, each_name)
-    with open(f"projekty/21_inputs/Output/ReadyToSend/letter_for_{each_name}.txt", mode="w") as completed_letter:
+    with open(f"02-Intermediate/h_inputs/Output/ReadyToSend/letter_for_{each_name}.txt", mode="w") as completed_letter:
         completed_letter.write(new_letter)
 
         # Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
