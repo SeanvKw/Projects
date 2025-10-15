38e1e367be3b44f7f2c7463e368963a51e31ff75
Adrian Rączka <raczka.tlen2@o2.pl>
2025-10-15 23:37:42 +0200

    Add implementation of dictionary comprehensions with student scores

---
+++ 02-Intermediate/l_Dictionary_Comprehensions/main.py
@@ -0,0 +1,10 @@
+''' new_dict = {new_key:new_value for (key, value) in dict.items() if condition}'''
+import random
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+
+students_scores = {student: random.randint(1, 100) for student in names}
+print(students_scores)
+
+passed_students = {student: score for student,
+                   score in students_scores.items() if int(score) >= 30}
+print(passed_students)
