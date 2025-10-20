18d9e0514d6c1d0ee16d0dd29a0330380544a953
Adrian Rączka <raczka.tlen2@o2.pl>
2025-10-21 00:27:10 +0200

    Fix calculation and update label references in Mile to Km Converter

--- 02-Intermediate/r_Tkinter_final/main.py
+++ 02-Intermediate/r_Tkinter_final/main.py
@@ -7,8 +7,8 @@ window.config(padx=20, pady=20)
 
 
 def miles_to_km():
-    to_miles = float(input_miles.get())
-    label_output.config(text=to_miles*1.6)
+    to_miles = int(input_miles.get())
+    label_result.config(text=round(to_miles*1.609344, 2))
 
 
 # Inputs
@@ -17,17 +17,17 @@ input_miles.grid(column=1, row=0)
 
 # Text/Outputs
 
-label = Label(text="Miles")
-label.grid(column=2, row=0)
+label_miles = Label(text="Miles")
+label_miles.grid(column=2, row=0)
 
-label = Label(text="is equal to")
-label.grid(column=0, row=1)
+label_equal = Label(text="is equal to")
+label_equal.grid(column=0, row=1)
 
-label_output = Label(text="0", width=10)
-label_output.grid(column=1, row=1)
+label_to_km = Label(text="Km")
+label_to_km.grid(column=2, row=1)
 
-label = Label(text="Km")
-label.grid(column=2, row=1)
+label_result = Label(text="0", width=10)
+label_result.grid(column=1, row=1)
 
 # Button
 
