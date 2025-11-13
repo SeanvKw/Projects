2fb7b9e5ccf9d5ed69390b031f5ea16789810e87
Adrian Rączka <raczka.tlen2@o2.pl>
2025-11-14 00:40:14 +0100

    Refactor letters list formatting and clean up commented code in password generator

--- 01-Beginner/c_Password_generator/password_generator.py
+++ 01-Beginner/c_Password_generator/password_generator.py
@@ -1,5 +1,6 @@
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+           'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -9,20 +10,20 @@ nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 # Easy Level
-#password = ""
+# password = ""
 #
-#for char in range(0, nr_letters):
+# for char in range(0, nr_letters):
 #    password += random.choice(letters)
 #
-#for char in range(0, nr_symbols):
+# for char in range(0, nr_symbols):
 #    password += random.choice(symbols)
 #
-#for char in range(0, nr_numbers):
+# for char in range(0, nr_numbers):
 #    password += random.choice(numbers)
 #
-#print(password)
+# print(password)
 
-# todo: Hard Level  How to shuffle items in list in python?
+# Hard Level
 
 password_random_list = []
 
