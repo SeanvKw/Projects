39c95b0b7856617c29b8e808ac64d68a31e04d06
Adrian Rączka <raczka.tlen2@o2.pl>
2025-12-01 01:42:07 +0100

    Add advanced Python decorator functions for user authentication

---
+++ 02-Intermediate/la_WebDevelopmentFlask/Beginner/advanced_decorators.py
@@ -0,0 +1,23 @@
+# Advanced Python Decorator Functions
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.is_logged_in = False
+
+
+def is_authenticated_decorator(func):
+    def wrapper(*args, **kwargs):
+        if args[0]._is_logged_in:
+            func(args[0])
+    return wrapper
+
+
+@is_authenticated_decorator
+def create_blog_post(user):
+    print(f"{user.name} just created a blog post!")
+
+
+new_user = User("Andrew")
+new_user.is_logged_in = True
+create_blog_post(new_user)  # No check if user is logged in
